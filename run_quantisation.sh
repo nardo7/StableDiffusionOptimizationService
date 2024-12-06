f63d0d2d09438ee7fd0cb5929cffb2bd811fa8ef
Alejandro Nardo <aleja@unibern-msgai-project.us-central1-c.c.msgai-projects.internal>
2024-12-06 20:30:55 +0000

    test

--- run_quantisation.sh
+++ run_quantisation.sh
@@ -1,9 +1,9 @@
 model=$1
 steps=$2
-python optimisation/quantization/quantize.py \
+python diffusion/optimisation/quantization/quantize.py \
   --model $model \
   --format int8 --batch-size 2 \
-  --calib-size 32 --collect-method global-min \
+  --calib-size 32 --collect-method global_min \
   --percentile 1.0 --alpha 0.8 \
   --quant-level 3.0 --n-steps $steps \
   --model-dtype Half \
